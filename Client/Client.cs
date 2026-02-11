645355db2ae1fe6e39bfdb099fe9ed6afbeceac8
vkatsel <vkatsel@kse.org.ua>
2026-02-11 20:44:34 +0200

    First drafts of client and server

--- Client/Client.cs
+++ Client/Client.cs
@@ -1,9 +1,37 @@
-﻿namespace Client;
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client;
 
 class Client
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        try
+        {
+            TcpClient client = new TcpClient(IPAddress.Loopback.ToString(), 1234);
+            NetworkStream ns = client.GetStream();
+            
+            Console.WriteLine("Connected to server!");
+
+            byte[] buffer = new byte[1024];
+            int bytesRead = ns.Read(buffer, 0, buffer.Length);
+            Console.WriteLine($"Server: {Encoding.UTF8.GetString(buffer, 0, bytesRead)}");
+
+            while (true)
+            {
+                Console.WriteLine("Enter message or 'exit': ");
+                string message = Console.ReadLine();
+                if (string.IsNullOrEmpty(message) || message == "exit") break;
+
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                ns.Write(data, 0, data.Length);
+            }
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine($"Connection failed: {e.Message}");
+        }
     }
 }
--- Server/Server.cs
+++ Server/Server.cs
@@ -1,9 +1,35 @@
-﻿namespace Server;
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server;
 
 class Server
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        IPAddress serverIp = IPAddress.Loopback;
+        int serverPort = 1234;
+        TcpListener server = new TcpListener(serverIp, serverPort);
+        
+        server.Start();
+
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            NetworkStream ns = client.GetStream();
+            
+            byte[] greeting = new byte[100];
+            greeting = Encoding.Default.GetBytes("Welcome to the server!");
+
+            ns.Write(greeting, 0, greeting.Length);
+
+            while (client.Connected)
+            {
+                byte[] msg = new byte[1024];
+                ns.Read(msg, 0, msg.Length);
+                Console.WriteLine(Encoding.UTF8.GetString(msg));
+            }
+        }
     }
 }
