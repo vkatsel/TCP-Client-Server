ccf5cb26f1105a2ce6b5c167c39a35c90746435d
vkatsel <vkatsel@kse.org.ua>
2026-02-11 20:06:00 +0200

    Initial commit

---
+++ Client/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace Client;
+
+class Client
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,9 @@
+﻿namespace Server;
+
+class Server
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
